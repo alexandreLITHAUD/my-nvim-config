f44ae35fa9e8d77dadd76555c9003c66d0c95b1f
Alexandre Lithaud <alexandre.ltd71@gmail.com>
2025-05-29 11:11:41 +0200

    Add flake for nvim conf

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Flake for my Neovim configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            neovim
+            python314
+            go
+            unzip
+            ripgrep
+            fd
+            curl
+            git
+            nodejs
+          ];
+
+          shellHook = ''
+            echo "Neovim environment is ready. Launch with: nvim"
+          '';
+        };
+      });
+}
