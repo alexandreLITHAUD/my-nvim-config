69aa14f3f7799b98a5cfd6d5fa8028e1864faf5a
Alexandre Lithaud <alexandre.ltd71@gmail.com>
2025-08-30 10:58:17 +0200

    simplify shortcuts

--- lua/keymaps/telescope.lua
+++ lua/keymaps/telescope.lua
@@ -1,9 +1,8 @@
 local M = {}
 
 M.keys = {
-	{ "<leader>f", group = "find" },
-	{ "<leader>fa", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-	{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+	{ "<leader>a", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+	{ "<leader>f", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
 }
 
 return M
