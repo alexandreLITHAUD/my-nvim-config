4da8ba90f105bfd1cb969bfe4edb2fffc72ac2e6
Alexandre Lithaud <alexandre.lithaud@non.se.com>
2025-11-30 13:46:08 +0100

    Fix copilot shenanigans

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,34 +1,40 @@
 return {
 	"zbirenbaum/copilot.lua",
-	requires = {
-		"copilotlsp-nvim/copilot-lsp", -- (optional) for NES functionality
-	},
 	cmd = "Copilot",
 	event = "InsertEnter",
 	config = function()
 		require("copilot").setup({
 			panel = {
-				enabled = false, -- Disable panel since you're using virtual text
+				enabled = false,
 				auto_refresh = false,
 			},
 			suggestion = {
 				enabled = true,
-				auto_trigger = true, -- Equivalent to manual = false
-				debounce = 75, -- Same as your idle_delay
+				auto_trigger = true,
+				debounce = 75,
 				keymap = {
-					accept = "<Tab>",
+					accept = false, -- Disable default Tab mapping
 					accept_word = false,
 					accept_line = false,
 					next = "<C-s>",
 					prev = "<C-q>",
-					dismiss = "<C-]>", -- Clear/dismiss suggestions
+					dismiss = "<C-]>",
 				},
 			},
 			filetypes = {
-				["*"] = true, -- Enable for all other filetypes by default
+				["*"] = true,
 			},
-			copilot_node_command = "node", -- Node.js version must be > 18.x
+			copilot_node_command = "node",
 			server_opts_overrides = {},
 		})
+
+		-- Custom Tab keybinding: accept Copilot or insert tab
+		vim.keymap.set("i", "<Tab>", function()
+			if require("copilot.suggestion").is_visible() then
+				require("copilot.suggestion").accept()
+			else
+				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+			end
+		end, { desc = "Accept Copilot suggestion or insert tab" })
 	end,
 }
